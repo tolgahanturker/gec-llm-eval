dbd1e813e29c1830d35fcecaffc3fac3fd5c1d41
tolgahanturker <tolgahanturker@gmail.com>
2025-06-16 09:07:00 +0300

    feat: data_loader m2 file load

--- data_loader.py
+++ data_loader.py
@@ -7,10 +7,25 @@ class DataLoader:
         sent_id = 0
         result = []
 
-        with Path(globals.INPUT_FILE_PATH).open(encoding="utf-8") as f:
-            for line in f:
-                line = line.strip()
-                sent_id += 1
-                result.append(dict(sentence_id = sent_id, test_sentence = line))
+        if globals.INPUT_FILE_PATH.endswith(".m2"):
+            with Path(globals.INPUT_FILE_PATH).open(encoding="utf-8") as file:
+                for line in file:
+                    line = line.strip()
+
+                    if line.startswith("S "):
+                        sent_id += 1
+                        result.append(dict(sentence_id = sent_id, test_sentence = line[2:]))
+        else:
+            with Path(globals.INPUT_FILE_PATH).open(encoding="utf-8") as file:
+                for line in file:
+                    line = line.strip()
+                    sent_id += 1
+                    result.append(dict(sentence_id = sent_id, test_sentence = line))
                 
+        
+        if globals.CONFIG["GENERAL"]["DEBUG"]:
+            print("####################")
+            print(f"Loaded {len(result)} sentences from {globals.INPUT_FILE_PATH}")
+
+
         return result
